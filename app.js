5ee4ddfc527c8237d1648453fb57fbfd8b0cd2af
xishanwawa <654189842@qq.com>
2017-01-08 00:18:34 +0800

    ok

--- app.js
+++ app.js
@@ -9,19 +9,41 @@ var bodyParser = require('body-parser');
 // var users = require('./routes/users');
 
 var routes = require('./routes/index');
+var settings = require('./settings');
+var flash = require('connect-flash');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({
+    /*db: settings.db,
+    host: settings.host,
+    port: settings.port*/
+    url: 'mongodb://localhost/blog'
+  })
+}));
 
 routes(app);
 
